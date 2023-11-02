e234f2b10660ed0f01e058cb43a69a101cb3e3a4
tonnidiaz <therealhackingknight@gmail.com>
2023-11-02 04:25:18 +0200

    rm rf.dart

--- lib/views/root/index.view.dart
+++ lib/views/root/index.view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:tu/views.dart';
 import 'package:tuned/views/about.dart';
-import 'package:tuned/views/root/widgets.view.dart';
 import 'package:tuned/widgets/sidebar.dart';
 import 'package:tu/tu.dart';
 
