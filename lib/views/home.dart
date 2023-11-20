e2052bb151dddabb8601873988bde162305bcd72
tonnidiaz <tonnidiazed@gmail.com>
2023-11-20 20:30:17 +0200

    ...

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tuned/main.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -6,7 +7,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text("Home")),
+        appBar: AppBar(title: Text(MainApp.appCtrl.title)),
         body: const SizedBox(
           width: double.infinity,
           child: Column(
