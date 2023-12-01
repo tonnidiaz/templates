accfb1da3222246acd0dc342a86c3c1b03ad879b
tonnidiaz <tonnidiazed@gmail.com>
2023-12-01 20:24:01 +0200

    backup/restore like a boss

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -10,6 +10,7 @@ import 'package:tu/functions.dart';
 import 'package:tu/tu.dart';
 import 'package:path/path.dart' as pth;
 import 'package:tuned/main.dart';
+import 'package:tuned/utils/functions.dart';
 
 import '../models/settings.dart';
 import '../utils/constants.dart';
@@ -22,115 +23,26 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-  final _formCtrl = MainApp.formCtrl;
-
-  _backupDB() async {
-    clog(isar.path);
-
-    try {
-      if (!(await requestStoragePermission())) return;
-      final saveDir = await FilePicker.platform.getDirectoryPath(
-        dialogTitle: "Save at",
-      );
-      if (saveDir == null) return;
-      clog("Save dir: $saveDir");
-      _formCtrl.setForm({"filename": isar.name.replaceAll(".isar", "")});
-
-      Get.dialog(TuDialogView(
-        isForm: true,
-        fields: [
-          const Text("Save as:"),
-          mY(10),
-          Obx(
-            () => TuFormField(
-              hint: "Filename:",
-              radius: 15,
-              onChanged: (val) {
-                _formCtrl.setFormField('filename', val);
-              },
-              value: _formCtrl.form['filename'],
-              suffix: const Text(".isar"),
-              required: true,
-              hasBorder: false,
-            ),
-          )
-        ],
-        okTxt: "Continue",
-        onOk: () async {
-          gpop();
-          final savePath =
-              pth.join(saveDir, "${_formCtrl.form['filename']}.isar");
-          clog("SAVED AT: $savePath");
-          if (await File(savePath).exists()) {
-            await File(savePath).delete();
-            clog("FILE DELETED");
-          }
-          await isar.copyToFile(savePath);
-          showToast("DB backed up").show(getCtx());
-        },
-      ));
-    } catch (e) {
-      clog(e);
-      showToast('Failed to backup db', isErr: true).show(Get.overlayContext!);
-    }
-  }
-
-  _restoreDB() async {
-    try {
-      if (!(await requestStoragePermission())) return;
-      final res = await FilePicker.platform.pickFiles();
-      if (res != null) {
-        final db = res.files[0];
-
-        if (db.name.endsWith('.isar')) {
-          final newDir = Directory(pth.join(
-              (await getExternalStorageDirectory())!.path, "db", "backup"));
-
-          final newFilename = "${isar.name}.backup.isar";
-          final dbCopy = File(db.path!);
-
-          // Create newDir if no exists
-          if (!(await newDir.exists())) {
-            await newDir.create(recursive: true);
-            clog("DIR CREATED");
-          }
-          await dbCopy.copy(pth.join(newDir.path, newFilename));
-          final mIsar = await Isar.open([SettingsSchema],
-              directory: newDir.path,
-              name: newFilename.replaceAll(".isar", ""));
-          mIsar.close();
-
-          showToast("DB RESTORED").show(getCtx());
-        } else {
-          showToast("Filename must end with .isar", isErr: true).show(getCtx());
-        }
-      }
-    } catch (e) {
-      clog(e);
-      showToast('Failed to restore db', isErr: true).show(Get.overlayContext!);
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     clog('HOME PAGE');
     return Scaffold(
         appBar: tuAppbar(title: Text(MainApp.appCtrl.title), actions: [
-          TuPopupBtn(
+          const TuPopupBtn(
             items: [
               PopupMenuItem(
                   child: TuPopupBtn(
                 items: [
                   PopupMenuItem(
-                    onTap: _backupDB,
-                    child: const Text("Backup"),
+                    onTap: TuIsar.backup,
+                    child: Text("Backup"),
                   ),
                   PopupMenuItem(
-                    onTap: _restoreDB,
-                    child: const Text("Restore"),
+                    onTap: TuIsar.restore,
+                    child: Text("Restore"),
                   ),
                 ],
-                child: const Text("Backup / Restore"),
+                child: Text("Backup / Restore"),
               ))
             ],
           )
